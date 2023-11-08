9b29a639c45cf02def1007c0caa01a4ebd6157d5
Erizon0 <42992935+Erizon0@users.noreply.github.com>
2023-11-08 15:15:18 +0100

    Update Opdracht_6.ino

--- Opdracht_6.ino
+++ Opdracht_6.ino
@@ -1,8 +1,9 @@
+//RGB led pin nummers defineren
 const int redPin = 3;
 const int bluePin = 5;
 const int greenPin = 6;
 
-
+//Pin setup om outputs te defineren en seriele poort
 void setup()
 {
   pinMode(redPin, OUTPUT);
@@ -11,16 +12,17 @@ void setup()
   Serial.begin(9600);
 }
 
+//loop om door alle verschillende kleuren van de RGB led te gaan
 void loop(){
   for(int i = 0; i <= 254; i= i+ 10){
   	for(int j = 0; j <= 254; j= j+10){
   		for(int k = 0; k <= 254; k= k+10){
-          analogWrite(redPin, i);
+        analogWrite(redPin, i);
     	  analogWrite(greenPin, j);
     	  analogWrite(bluePin, k);
     	  Serial.println(i);
-          Serial.println(j);
-          Serial.println(k);
+        Serial.println(j);
+        Serial.println(k);
     	  delay(50);
         }
     }
