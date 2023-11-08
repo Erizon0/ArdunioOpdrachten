e63ceeb765dc8206e1aba26a9d57fe55205927ea
Erizon0 <42992935+Erizon0@users.noreply.github.com>
2023-11-08 15:06:53 +0100

    Update Opdracht_3.ino

--- Opdracht_3.ino
+++ Opdracht_3.ino
@@ -1,14 +1,14 @@
-// C++ code
-//
-
+//initiele waarde voor variable
 bool pressed = false;
 
+//Pin setup om in en outputs te defineren
 void setup()
 {
   pinMode(11, OUTPUT);
   pinMode(10, INPUT);
 }
 
+//loop om te kijken of de knop is ingedrukt en een ledje te aan of uit te zetten op een druk
 void loop()
 {
   if((digitalRead(10) == true) && (pressed == false)){
