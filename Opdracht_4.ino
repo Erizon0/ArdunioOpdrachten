dc59a179f8742c691734da20ba5bfc85576ba76f
Erizon0 <42992935+Erizon0@users.noreply.github.com>
2023-11-08 15:11:40 +0100

    Update Opdracht_4.ino

--- Opdracht_4.ino
+++ Opdracht_4.ino
@@ -1,8 +1,9 @@
-const int LED_COUNT = 7; // number of LEDs in the array
-const int LED_PINS[LED_COUNT] = {3,4,5,6,7,8,9}; // array of LED pins
-const int POT_PIN = A0; // potentiometer input pin
-int ledOn = -1; // index of the LED that is currently on (-1 indicates no LED is on)
+const int LED_COUNT = 7; // hoeveelheid aan leds totaal
+const int LED_PINS[LED_COUNT] = {3,4,5,6,7,8,9}; // lijst met de aansluitingen voor de ledjes
+const int POT_PIN = A0; // potmeter input pin
+int ledOn = -1; // index om aan te geven welke led op dit moment aan staat (-1 betekend dat geen led aanstaat)
 
+//Pin setup om outputs te defineren
 void setup() {
   // set all LED pins to output mode
   for (int i = 0; i < LED_COUNT; i++) {
@@ -11,25 +12,25 @@ void setup() {
 }
 
 void loop() {
-  // read the value of the potentiometer
+  // potmeter uitlezen
   int potValue = analogRead(POT_PIN);
   
-  // map the potentiometer value to a LED index between 0 and 6
+  // de waarde van de potmeter zetten op de hoeveelheid leds
   int ledIndex = map(potValue, 0, 1023, 0, LED_COUNT - 1);
   
-  // turn on the selected LED and turn off all others
+  // het geselecteerde ledje aanzetten en de rest uit op basis van de waarde
   if (ledIndex != ledOn) {
-    // turn off all LEDs
+    // alle leds uitezetten
     for (int i = 0; i < LED_COUNT; i++) {
       digitalWrite(LED_PINS[i], LOW);
     }
-    // turn on the selected LED
+    // geselecteerde led aanzetten
     digitalWrite(LED_PINS[ledIndex], HIGH);
-    // turn on all LEDs below the selected LED
+    // alle leds onder de geselecteerde led aanzetten
     for (int i = ledIndex + 1; i < LED_COUNT; i++) {
       digitalWrite(LED_PINS[i], HIGH);
     }
-    // update ledOn variable
+    // de ledOn updaten naar de huidige led die aanstaat
     ledOn = ledIndex;
   }
 }
